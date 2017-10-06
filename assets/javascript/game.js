28b4630ebb5d6406937c9810a2daf2a038e4285f
dwc3k5 <dwc1608@gmail.com>
2017-10-06 09:08:46 -0500

    functioning game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,13 @@
 //check your console in the inspect to make sure you linked your javascript appropriately
 
+var hullStrength = 100;
+var hullStrengthMax = 100;
+var booty = 20;
 
+document.getElementById("booty").innerHTML = booty + " gold";
+// document.getElementById()
 /*----------alphabet generator-------------------------------------------------*/
-var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","space"];
+var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",""];
 for (var j = 0; j < alpha.length; j++) {
   var letterRetrieve = 0;
   document.getElementById("alphaHolder").innerHTML = alpha.join(" ");
@@ -23,13 +28,13 @@ document.getElementById("guessContainer").innerHTML = categories[categoriesIndex
 /*--------generating a random word based on the category-----------------------*/
 var wordBank;
 if (categoriesIndex === 0) {
-  wordBank = ["first mate", "blackbeard", "redbeard", "davey jones", "captain hook"];
+  wordBank = ["first_mate", "blackbeard", "redbeard", "daveyjones", "captainhook", "jacksparrow"];
 } else if (categoriesIndex === 1) {
-  wordBank = ["booty", "peg Leg", "shark bait", "plunder"];
+  wordBank = ["booty", "pegleg", "sharkbait", "plunder", "treasuremap"];
 } else if (categoriesIndex === 2) {
-  wordBank = ["crows nest", "captains quarters"];
+  wordBank = ["crowsnest", "captains quarters", "lagoon", "cove"];
 } else if (categoriesIndex === 3) {
-  wordBank = ["yarr", "yoho", "shiver me timbers"];
+  wordBank = ["yarr", "yoho", "shivermetimbers","" ];
 }
 var wordBankIndex = Math.floor(Math.random() * wordBank.length);
 var mysteryWord = wordBank[wordBankIndex];
@@ -45,7 +50,7 @@ for (var j = 0; j < mysteryWordArray.length; j++) {
 }
 
 
-/*Converts mysteryWordArray to a mysteryWordHidden-----------------------------*/
+/*Converts mysteryWordArray to an array with a "_" for each lettery in the word*/
 //DEV note: this is converting both hidden and array to be blank
 var mysteryWordHidden = mysteryWordArray;
 // console.log("mysteryWordArray " + mysteryWordArray);
@@ -66,6 +71,7 @@ document.onkeyup = function(event) {
   document.getElementById("botLeft").innerHTML = guessKey;
   document.getElementById("botRight").innerHTML = guessKey;
   document.getElementById("dynamicText").innerHTML = "Fire!!!";
+  // document.getElementById('name-logo').style.img = "url('my.gif?v=" + new Date().valueOf() + "')";
 
   /*section activates wordbank-------------------------------------------------*/
 
@@ -81,18 +87,29 @@ document.onkeyup = function(event) {
 
 /*DETERMINE IF LETTER IS IN hiddenWord-----------------------------------------*/
     console.log("guessKey " + guessKey);
+    console.log("mysteryWordArray2" + mysteryWordArray2);
+    console.log("mysteryWordArray" + mysteryWordArray);
     if(mysteryWordArray2.indexOf(guessKey) !== -1 ){
        for(var i=0; i<mysteryWordArray2.length; i++){
-         console.log("mysteryWordArray[i] " + mysteryWordArray2[i]);
           if( guessKey === mysteryWordArray2[i] ){
           mysteryWordArray[i] = guessKey;
-      document.getElementById("wordContainer").innerHTML = mysteryWordArray.join(" ");
+      document.getElementById("wordContainer").innerHTML = mysteryWordArray.join(" ");}}
+    }else{
+      hullStrength -= 2;
+      document.getElementById("hullStrength").innerHTML= hullStrength + "%";
+      // document.getElementById("healthBar").width = hullStrength-hullStrengthMax;
 
-
-
-}
-
-}
-}
-  // }
+    }
 };
+//
+// if( hullStrength >= 66){
+//   document.getElementById("healthBar").style.color = "blue";
+// }else if(hullStrength >=33){
+//   document.getElementById("healthBar").style.color = "yellow";
+// }else{
+//   document.getElementById("healthBar").style.color = "red";
+// }
+
+
+
+// set timeout
